bf30076202bf4cad18ac6e7abb53d8f77470f40e
Shken <gorob4ik1983@gmail.com>
2022-12-20 19:34:58 +0100

    fix

--- src/sharedModules/header/index.js
+++ src/sharedModules/header/index.js
@@ -3,24 +3,24 @@ import logo from '../../images/logo-head.svg';
 import Button from 'react-bootstrap/Button';
 
 const Index = () => {
-  return (
-    <header>
-      <div className="container container-sm">
-        <div className="row align-items-center">
-          <div className="col-4">
-            <div className="nabvar-brand">
-              <a href="/">
-                <img src={logo} alt="logo"/>
-              </a>
+    return (
+        <header>
+            <div className="container container-sm">
+                <div className="row align-items-center">
+                    <div className="col-4">
+                        <div className="nabvar-brand">
+                            <a href="/">
+                                <img src={logo} alt="logo" />
+                            </a>
+                        </div>
+                    </div>
+                    <div className="col-8 text-right">
+                        <Button variant="theme-outline">CONNECT WALLET</Button>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div className="col-8 text-right">
-            <Button variant="theme-outline">CONNECT WALLET</Button>
-          </div>
-        </div>
-      </div>
-    </header>
-  )
+        </header>
+    )
 }
 
 export default Index;
